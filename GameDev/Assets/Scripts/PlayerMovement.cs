865771646ec3db09565522c4d79332e7b92fc939
CianOBRIENuk <56305201+CianOBRIENuk@users.noreply.github.com>
2023-03-13 00:47:00 +0000

    Player movement (#2)
    * Update PlayerMovement.cs
    
    * Update default-2021.dwlt

--- GameDev/Assets/Scripts/PlayerMovement.cs
+++ GameDev/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float movementSpeed = 1f;
+    public Vector2 movement;
+    public Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
     }
 }

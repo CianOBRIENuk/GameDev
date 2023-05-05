6f2fcd432c98d0ce9c048fb734dc2e745d3ed949
CianOBRIENuk <56305201+CianOBRIENuk@users.noreply.github.com>
2023-05-05 22:38:36 +0100

    Added camera control

---
+++ GameDev/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform target; // set to the Player GameObject
+
+    public Vector2 worldPosMin = new Vector2(-10f, -10f);
+    public Vector2 worldPosMax = new Vector2(10f, 10f);
+
+    void Update()
+    {
+        if (target != null)
+        {
+            Vector2 targetPos = target.transform.position;
+
+            float x = Mathf.Clamp(targetPos.x, worldPosMin.x, worldPosMax.x);
+            float y = Mathf.Clamp(targetPos.y, worldPosMin.y, worldPosMax.y);
+
+            transform.position = new Vector3(x, y, transform.position.z);
+        }
+    }
+}
+     
